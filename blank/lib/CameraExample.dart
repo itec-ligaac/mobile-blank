0490e11e00e6725e2d343cd9822d9d7c257ad3ee
Gabjea <43420432+Gabjea@users.noreply.github.com>
2021-03-27 19:45:23 +0200

    first_commit

---
+++ blank/lib/CameraExample.dart
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2019-2021 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:here_sdk/core.dart';
+import 'package:here_sdk/mapview.dart';
+
+// This example shows how to animate the MapCamera from A to B with the Camera's flyTo()-method
+class CameraExample {
+  HereMapController _hereMapController;
+  final double distanceToEarthInMeters = 5000;
+  MapPolygon centerMapCircle;
+
+  CameraExample(HereMapController hereMapController) {
+    _hereMapController = hereMapController;
+
+    // Set initial map center to a location in Berlin.
+    GeoCoordinates mapCenter = GeoCoordinates(52.530932, 13.384915);
+    _hereMapController.camera.lookAtPointWithDistance(mapCenter, distanceToEarthInMeters);
+    _setNewMapCircle(mapCenter);
+  }
+
+  void move() {
+    GeoCoordinates newTarget = _createRandomGeoCoordinatesNearby();
+
+    // Indicate the new map center with a circle.
+    _setNewMapCircle(newTarget);
+
+    _hereMapController.camera.flyToWithOptions(newTarget, MapCameraFlyToOptions.withDefaults());
+  }
+
+  void _setNewMapCircle(GeoCoordinates geoCoordinates) {
+    if (centerMapCircle != null) {
+      _hereMapController.mapScene.removeMapPolygon(centerMapCircle);
+    }
+    centerMapCircle = _createMapCircle(geoCoordinates);
+    _hereMapController.mapScene.addMapPolygon(centerMapCircle);
+  }
+
+  MapPolygon _createMapCircle(GeoCoordinates geoCoordinates) {
+    double radiusInMeters = 70;
+    GeoCircle geoCircle = GeoCircle(geoCoordinates, radiusInMeters);
+
+    GeoPolygon geoPolygon = GeoPolygon.withGeoCircle(geoCircle);
+    Color fillColor = Color.fromARGB(255, 0, 138, 161);
+    MapPolygon mapPolygon = MapPolygon(geoPolygon, fillColor);
+
+    return mapPolygon;
+  }
+
+  GeoCoordinates _createRandomGeoCoordinatesNearby() {
+    GeoBox geoBox = _hereMapController.camera.boundingBox;
+    if (geoBox == null) {
+      // Happens only when map is not fully covering the viewport.
+      return GeoCoordinates(52.530932, 13.384915);
+    }
+
+    GeoCoordinates northEast = geoBox.northEastCorner;
+    GeoCoordinates southWest = geoBox.southWestCorner;
+
+    double minLat = southWest.latitude;
+    double maxLat = northEast.latitude;
+    double lat = _getRandom(minLat, maxLat);
+
+    double minLon = southWest.longitude;
+    double maxLon = northEast.longitude;
+    double lon = _getRandom(minLon, maxLon);
+
+    int sign1 = math.Random().nextBool() ? 1 : -1;
+    int sign2 = math.Random().nextBool() ? 1 : -1;
+
+    return GeoCoordinates(lat + 0.05 * sign1, lon + 0.05 * sign2);
+  }
+
+  double _getRandom(double min, double max) {
+    return min + math.Random().nextDouble() * (max - min);
+  }
+}
---
+++ blank/lib/pages/LoginPage.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../ProgressHUD.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool hidePassword = true;
+  bool isApiCallProcess = false;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+
+  TextEditingController name = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+
+  Future<List> senddata() async {
+    final response =
+        await http.post("https://tiara2.mt2.ro/insertdata.php", body: {
+      "name": "test1",
+      "email": email.text,
+      "password": password.text,
+    });
+  }
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    super.initState();
+    // loginRequestModel = new LoginRequestModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressHUD(
+      child: _uiSetup(context),
+      inAsyncCall: isApiCallProcess,
+      opacity: 0.3,
+    );
+  }
+
+  Widget _uiSetup(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Theme.of(context).accentColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 85, horizontal: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Theme.of(context).primaryColor,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Theme.of(context).hintColor.withOpacity(0.2),
+                          offset: Offset(0, 10),
+                          blurRadius: 20)
+                    ],
+                  ),
+                  child: Form(
+                    key: globalFormKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 25),
+                        Text(
+                          "Login",
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        SizedBox(height: 20),
+                        new TextFormField(
+                          controller: email,
+                          keyboardType: TextInputType.emailAddress,
+                          //  onSaved: (input) => loginRequestModel.email = input,
+                          validator: (input) => !input.contains('@')
+                              ? "Email Id should be valid"
+                              : null,
+                          decoration: new InputDecoration(
+                            hintText: "Email Address",
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context)
+                                        .accentColor
+                                        .withOpacity(0.2))),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).accentColor)),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        new TextFormField(
+                          controller: password,
+                          style:
+                              TextStyle(color: Theme.of(context).accentColor),
+                          keyboardType: TextInputType.text,
+                          //onSaved: (input) =>
+                          //    loginRequestModel.password = input,
+                          validator: (input) => input.length < 3
+                              ? "Password should be more than 3 characters"
+                              : null,
+                          obscureText: hidePassword,
+                          decoration: new InputDecoration(
+                            hintText: "Password",
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context)
+                                        .accentColor
+                                        .withOpacity(0.2))),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).accentColor)),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Theme.of(context).accentColor,
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  hidePassword = !hidePassword;
+                                });
+                              },
+                              color: Theme.of(context)
+                                  .accentColor
+                                  .withOpacity(0.4),
+                              icon: Icon(hidePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        FlatButton(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 80),
+                          onPressed: () {
+                            senddata();
+                            final snackBar = SnackBar(
+                                        content: Text("Login Successful"));
+                                    scaffoldKey.currentState
+                                        .showSnackBar(snackBar);
+                          },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Theme.of(context).accentColor,
+                          shape: StadiumBorder(),
+                        ),
+                        SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  
+}
